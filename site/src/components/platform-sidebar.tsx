4979581eaed8ded9be81c68b74d3e71e2813e04d
mbeka02 <antonymbeka@gmail.com>
2025-03-17 14:47:08 +0300

    fix:mobile device sidebar nav

--- site/src/components/platform-sidebar.tsx
+++ site/src/components/platform-sidebar.tsx
@@ -38,7 +38,7 @@ export function PlatformSidebar() {
   return (
     <div
       className={cn(
-        "rounded-md flex flex-col md:flex-row bg-gray-100 dark:bg-neutral-800   border border-neutral-200 dark:border-neutral-700 overflow-hidden",
+        "rounded-md  flex flex-col md:flex-row bg-gray-100 dark:bg-neutral-800   border border-neutral-200 dark:border-neutral-700 overflow-hidden",
         "h-screen",
       )}
     >
