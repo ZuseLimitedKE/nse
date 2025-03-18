caf02ded0538482ae71bc8ab052d3acabf6f4873
mbeka02 <antonymbeka@gmail.com>
2025-03-18 22:06:26 +0300

    fix:linting error

--- site/src/contract/index.ts
+++ site/src/contract/index.ts
@@ -1,20 +1,21 @@
 import { Errors, MyError } from "@/constants/errors";
 
 interface CreateStockTokenArgs {
-    symbol: string,
-    name: string
+  symbol: string;
+  name: string;
 }
 
 export class SmartContract {
-    async createStockToken(args: CreateStockTokenArgs): Promise<string> {
-        try {
-            //TODO: Call function for minting stock
-            return "address";
-        } catch(err) {
-            console.log("Error creating stock token", err);
-            throw new MyError(Errors.NOT_CREATE_STOCK_TOKEN);
-        }
+  async createStockToken(args: CreateStockTokenArgs): Promise<string> {
+    try {
+      //TODO: Call function for minting stock
+      console.log(args); // MAKE SURE YOU HAVE NO UNUSED VARIABLES - Anthony
+      return "address";
+    } catch (err) {
+      console.log("Error creating stock token", err);
+      throw new MyError(Errors.NOT_CREATE_STOCK_TOKEN);
     }
+  }
 }
 
 const smartContract = new SmartContract();
