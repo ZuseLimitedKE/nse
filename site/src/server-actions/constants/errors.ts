14d6aa5b3606396b9a6a48f17a5ef27dd61c6029
romannjoroge <roman.njoroge@njuguna.com>
2025-03-18 17:06:21 +0300

    feat: Splitting main create stock function from function to create stock on mongo since I also want to create token for stock

--- site/src/server-actions/constants/errors.ts
+++ site/src/server-actions/constants/errors.ts
@@ -7,5 +7,6 @@ export class MyError extends Error {
 
 export enum Errors {
     INVALID_SETUP="Set up environment variables correctly",
-    NOT_CREATE_STOCK = "Could not create stock"
+    NOT_CREATE_STOCK = "Could not create stock",
+    NOT_CREATE_STOCK_DB = "Could not create stock in DB"
 }
--- site/src/server-actions/creations/stocks.ts
+++ site/src/server-actions/creations/stocks.ts
@@ -3,9 +3,21 @@ import { STOCKS, STOCKS_COLLECTION } from "../db/collections";
 
 export async function createStock(args: STOCKS) {
     try {
-        await STOCKS_COLLECTION.insertOne(args);
+        // Create stock in smart contract
+
+        // Create stock in DB
+        await createStockInDB(args);
     } catch(err) {
         console.log("Error creating stock", err);
         throw new MyError(Errors.NOT_CREATE_STOCK);
+    }
+}
+
+async function createStockInDB(args: STOCKS) {
+    try {
+        await STOCKS_COLLECTION.insertOne(args);
+    } catch(err) {
+        console.log("Error creating stock", err);
+        throw new MyError(Errors.NOT_CREATE_STOCK_DB);
     }
 }
