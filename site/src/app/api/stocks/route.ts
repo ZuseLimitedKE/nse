7f4e0ae4c6f9f755c97da9f3c6909ff725db6f02
romannjoroge <roman.njoroge@njuguna.com>
2025-03-18 20:56:50 +0300

    feat: Tokenize stock route done

--- site/src/app/api/stocks/route.ts
+++ site/src/app/api/stocks/route.ts
@@ -1,3 +1,4 @@
+import { Success } from "@/constants/success";
 import { tokenizeStockSchema } from "@/constants/types";
 import smartContract from "@/contract";
 import database from "@/db";
@@ -8,6 +9,7 @@ export async function POST(request: Request) {
     const parsed = tokenizeStockSchema.safeParse(res);
     if (parsed.success) {
         await createStock(parsed.data, database, smartContract);
+        return Response.json({message: Success.STOCK_TOKENIZED});
     } else {
         const errors = parsed.error.issues.map((i) => i.message);
         return Response.json({error: [errors]}, {status: 400})
