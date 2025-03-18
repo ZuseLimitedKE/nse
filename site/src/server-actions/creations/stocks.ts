5697157372ee5df130698b0f8de36120cc1be7cd
romannjoroge <roman.njoroge@njuguna.com>
2025-03-18 17:25:01 +0300

    feat: Updating server-actions creations to use the smart contract and db classes

--- site/src/server-actions/creations/stocks.ts
+++ site/src/server-actions/creations/stocks.ts
@@ -1,23 +1,22 @@
-import { Errors, MyError } from "../constants/errors";
-import { STOCKS, STOCKS_COLLECTION } from "../db/collections";
+import { SmartContract } from "@/contract";
+import { Errors, MyError } from "@/constants/errors";
+import { MyDatabase } from "@/db";
+export interface CreateStock {
+    symbol: string,
+    name: string,
+    todayPrice: number,
+    stockID: string | undefined
+}
 
-export async function createStock(args: STOCKS) {
+export async function createStock(args: CreateStock, database: MyDatabase, smarContract: SmartContract) {
     try {
         // Create stock in smart contract
-
+        const tokenAddress = await smarContract.createStockToken({symbol: args.symbol, name: args.name});
         // Create stock in DB
-        await createStockInDB(args);
+        await database.createStockInDB({...args, tokenAddress});
     } catch(err) {
         console.log("Error creating stock", err);
         throw new MyError(Errors.NOT_CREATE_STOCK);
     }
 }
 
-async function createStockInDB(args: STOCKS) {
-    try {
-        await STOCKS_COLLECTION.insertOne(args);
-    } catch(err) {
-        console.log("Error creating stock", err);
-        throw new MyError(Errors.NOT_CREATE_STOCK_DB);
-    }
-}
