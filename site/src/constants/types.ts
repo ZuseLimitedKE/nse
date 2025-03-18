a62a65ecf261a3bced8d15ab76c2708a00598802
romannjoroge <roman.njoroge@njuguna.com>
2025-03-18 20:56:21 +0300

    fix: Making stock_id optional

--- site/src/constants/types.ts
+++ site/src/constants/types.ts
@@ -4,7 +4,7 @@ import { Errors } from "./errors";
 export const tokenizeStockSchema = z.object({
     symbol: z.string({message: Errors.INVALID_SYMBOL}),
     name: z.string({message: Errors.INVALID_NAME}),
-    stockID: z.string({message: Errors.INVALID_STOCK_ID})
+    stockID: z.string({message: Errors.INVALID_STOCK_ID}).optional()
 });
 
 export type TokenizeStock = z.infer<typeof tokenizeStockSchema>;
