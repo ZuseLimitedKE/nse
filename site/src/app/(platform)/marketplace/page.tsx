2ebc4eec1e83c3d4b6191859fbab1085e49cd3ab
mbeka02 <antonymbeka@gmail.com>
2025-03-22 22:01:07 +0300

    made marketplace a dynamic route

--- site/src/app/(platform)/marketplace/page.tsx
+++ site/src/app/(platform)/marketplace/page.tsx
@@ -2,6 +2,7 @@ import { getQueryClient } from "@/context/get-query-client";
 import { HydrationBoundary, dehydrate } from "@tanstack/react-query";
 import { Stocks } from "./_components/stocks";
 import { getStocks } from "@/server-actions/stocks/getStocks";
+export const dynamic = "force-dynamic";
 export default async function MarketPlacePage() {
   const queryClient = getQueryClient();
   await queryClient.prefetchQuery({
