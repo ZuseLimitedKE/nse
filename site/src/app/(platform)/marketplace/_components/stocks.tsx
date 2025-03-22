e69264b9b7f5e9f8c78eb68d5bfaf03d4c8166fa
mbeka02 <antonymbeka@gmail.com>
2025-03-22 19:12:32 +0300

    fixes

--- site/src/app/(platform)/marketplace/_components/stocks.tsx
+++ site/src/app/(platform)/marketplace/_components/stocks.tsx
@@ -1,8 +1,8 @@
 "use client";
-// import { Skeleton } from "@/components/ui/skeleton";
-// import { useQuery } from "@tanstack/react-query";
-// import { getStocks } from "@/server-actions/stocks/getStocks";
-import { usePolling } from "@/hooks/usePolling";
+import { Skeleton } from "@/components/ui/skeleton";
+import { useQuery } from "@tanstack/react-query";
+import { getStocks } from "@/server-actions/stocks/getStocks";
+// import { usePolling } from "@/hooks/usePolling";
 interface Stocks {
   id: string;
   symbol: string;
@@ -10,23 +10,23 @@ interface Stocks {
   price: number;
   change: number;
 }
-export function Stocks({ stocks }: { stocks: Stocks[] }) {
-  // const { data: stocks, isLoading } = useQuery({
-  //   queryKey: ["stocks"],
-  //   queryFn: getStocks,
-  //   // enable polling
-  //   refetchInterval: 3000,
-  // });
-  // if (isLoading) {
-  //   return (
-  //     <div className="w-full grid gap-3">
-  //       {Array.from({ length: 9 }).map((_, i) => (
-  //         <Skeleton key={i} className="w-24 h-8 bg-gray-200" />
-  //       ))}
-  //     </div>
-  //   );
-  // }
-  usePolling(15000);
+export function Stocks(/*{ stocks }: { stocks: Stocks[] }*/) {
+  const { data: stocks, isLoading } = useQuery({
+    queryKey: ["stocks"],
+    queryFn: getStocks,
+    // enable polling
+    refetchInterval: 8000,
+  });
+  if (isLoading) {
+    return (
+      <div className="w-full grid gap-3">
+        {Array.from({ length: 9 }).map((_, i) => (
+          <Skeleton key={i} className="w-24 h-8 bg-gray-200" />
+        ))}
+      </div>
+    );
+  }
+  // usePolling(15000);
   return (
     <div className="w-full grid  justify-between">
       {stocks?.map((stock, index) => (
--- site/src/app/(platform)/marketplace/page.tsx
+++ site/src/app/(platform)/marketplace/page.tsx
@@ -1,19 +1,18 @@
-// import { getQueryClient } from "@/context/get-query-client";
-// import { HydrationBoundary, dehydrate } from "@tanstack/react-query";
+import { getQueryClient } from "@/context/get-query-client";
+import { HydrationBoundary, dehydrate } from "@tanstack/react-query";
 import { Stocks } from "./_components/stocks";
 import { getStocks } from "@/server-actions/stocks/getStocks";
-
 export default async function MarketPlacePage() {
-  // const queryClient = getQueryClient();
-  // await queryClient.prefetchQuery({
-  //   queryKey: ["stocks"],
-  //   queryFn: getStocks,
-  // });
-  const stocks = await getStocks();
-  console.log(stocks);
+  const queryClient = getQueryClient();
+  await queryClient.prefetchQuery({
+    queryKey: ["stocks"],
+    queryFn: getStocks,
+  });
+  // const stocks = await getStocks();
+  // console.log(stocks);
   return (
-    // <HydrationBoundary state={dehydrate(queryClient)}>
-    <Stocks stocks={stocks} />
-    // </HydrationBoundary>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Stocks /*stocks={stocks}*/ />
+    </HydrationBoundary>
   );
 }
