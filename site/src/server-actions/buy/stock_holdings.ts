4e5a3eecfbb8eb2991996c37e556e1fac0bb0e00
romannjoroge <roman.njoroge@njuguna.com>
2025-03-23 16:21:36 +0300

    feat: server action to store stock purchase

--- site/src/server-actions/buy/stock_holdings.ts
+++ site/src/server-actions/buy/stock_holdings.ts
@@ -1,10 +1,12 @@
 "use server"
 import { Errors, MyError } from "@/constants/errors";
 import { StoreStockPurchase } from "@/constants/types";
+import database from "@/db";
 
 export async function store_stock_purchase(args: StoreStockPurchase) {
     try {
         // Store in db
+        await database.storeStockPurchase(args);
     } catch(err) {
         console.log("Error storing stock purchase", err);
         throw new MyError(Errors.NOT_STORE_STOCK_PURCHASE);
