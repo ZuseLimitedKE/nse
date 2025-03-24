9dc6756b83daad3b35e7884ccb7f10afc70a9162
Felista-Njeri <njeerikamau2@gmail.com>
2025-03-24 14:49:43 +0300

    Add config details to hardhat.config file

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -1,7 +1,57 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "@typechain/hardhat";
+import "hardhat-deploy";
+import "dotenv/config";
+
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "api-key";
+//const SEPOLIA_ETHERSCAN_API_KEY = process.env.SEPOLIA_ETHERSCAN_API_KEY || "api-key";
+const WALLET_PRIVATE_KEY = process.env.WALLET_PRIVATE_KEY;
 
 const config: HardhatUserConfig = {
+  defaultNetwork: "hardhat",
+  namedAccounts: {
+    deployer: {
+      default: 0, // First account in the list
+    },
+  },
+  typechain: {
+    outDir: "typechain-types",
+    target: "ethers-v6",
+  },
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    "lisk-sepolia": {
+      url: "https://rpc.sepolia-api.lisk.com",
+      accounts: [WALLET_PRIVATE_KEY],
+      gasPrice: 1000000000,
+    },
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY_SEPOLIA}`,
+      accounts: [WALLET_PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: {
+      sepolia: ETHERSCAN_API_KEY || "",
+      "lisk-sepolia": "123",
+    },
+    customChains: [
+      {
+        network: "lisk-sepolia",
+        chainId: 4202,
+        urls: {
+          apiURL: "https://sepolia-blockscout.lisk.com/api",
+          browserURL: "https://sepolia-blockscout.lisk.com",
+        },
+      },
+    ],
+  },
+  sourcify: {
+    enabled: false,
+  },
   solidity: "0.8.28",
 };
 
