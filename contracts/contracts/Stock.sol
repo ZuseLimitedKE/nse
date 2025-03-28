4df7d1abaabed75c6dcd66fcff7e6d21f2ab80e5
PatrickOjiambo <pashrick237@students.uonbi.ac.ke>
2025-03-28 14:37:03 +0300

    feat: buy stocks

--- contracts/contracts/Stock.sol
+++ contracts/contracts/Stock.sol
@@ -1,82 +1,29 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.27;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract StockTokenization is ERC20, Ownable {
-    
+contract StockTokenization is ERC1155, Ownable {
+    constructor(string memory uri) ERC1155(uri) {}
     struct Stock{
-        uint256 stockId;
-        string name;
+        string stockId;
+        uint256 totalSupply;
         string symbol;
-        string identifier;
-        uint256 totalShares;
-        uint256 sharesSold;
-        uint256 sharePrice;
-        address issuer;
-        bool isActive;
+        string name; 
     }
-
-    mapping(uint256 => Stock) public stocks; //mapping to store the stocks by their id
-
-    uint256 public nextStockId = 1; //counter for stock ids
-
-    //event to log stock tokenization
-    event StockTokenized(
-        uint256 stockId,
-        string name,
-        string symbol,
-        string identifier,
-        uint256 totalShares,
-        uint256 sharePrice,
-        address issuer
-    );
-
-    //event to log price updates
-    event PriceUpdated(uint256 stockId, uint256 newPrice);
-    
-    //constructor to initialize the ERC20 token and set the admin address
-    constructor() ERC20("StockToken", "STK") Ownable(msg.sender) {}
-
-    //function to tokenize a new stock
-    function tokenizeStock( string memory _name, string memory _symbol, string memory _identifier, uint256 _totalShares, uint256 _sharePrice) external onlyOwner {
-        require(_totalShares > 0, "Total shares must be greater than 0");
-        require(_sharePrice > 0, "Share price must be greater than 0");
-
-        //new stock ID
-        uint256 stockId = nextStockId++;
-
-        //create new stock struct and store it in the mapping
-        stocks[stockId] = Stock({
-            stockId: stockId,
-            name: _name,
-            symbol: _symbol,
-            identifier: _identifier,
-            totalShares: _totalShares,
-            sharesSold: 0,
-            sharePrice: _sharePrice,
-            issuer: owner(),
-            isActive: true
-        });
-
-        //mint the total shares to the admin address
-        _mint(owner(), _totalShares);
-        
-        //emit an event to log the tokenization
-        emit StockTokenized(stockId, _name, _symbol, _identifier, _totalShares, _sharePrice, owner());
-    }
-
-    //function to update the price of a stock (called by the backend)
-    function updateStockPrice(uint256 _stockId, uint256 _newPrice) external onlyOwner {
-        require(_newPrice > 0, "Price must be greater than 0");
-        require(stocks[_stockId].isActive, "Stock is not active");
-
-        //update the stock price
-        stocks[_stockId].sharePrice = _newPrice;
-
-        //emit an event to log the price update
-        emit PriceUpdated(_stockId, _newPrice);
-    }
-
+    mapping(string => Stock) public stocks;
+   function createStock(string memory shareId, string memory symbol, uint256 initialSupply, string memory tokenName)public onlyOwner{
+    require (bytes(stocks[shareId].symbol).length == 0, "Stock already exists");
+    Stock storage stock = stocks[shareId];
+    stock.stockId = shareId;
+    stock.totalSupply = initialSupply;
+    stock.symbol = symbol;
+    stock.name = tokenName;
+    _mint(address(this), shareId, initialSupply, "");
+   }
+   function getSymbol(string memory shareId) public view returns(string memory){
+       return stocks[shareId].symbol;
+   }
 }
