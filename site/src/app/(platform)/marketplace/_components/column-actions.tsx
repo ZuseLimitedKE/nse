a5addcc3db3b39378698f4f9456945f899d9f980
mbeka02 <antonymbeka@gmail.com>
2025-03-29 23:51:41 +0300

    fix

--- site/src/app/(platform)/marketplace/_components/column-actions.tsx
+++ site/src/app/(platform)/marketplace/_components/column-actions.tsx
@@ -61,7 +61,7 @@ export function ColumnActions({ entry }: { entry: StockData }) {
   // }, [quantity, entry.price, form.setValue]);
   // Handle form submission
   const onSubmit = async (data: FormValues) => {
-    const finalAmount = entry.price * quantity; // Calculate amount dynamically
+    const finalAmount = Math.ceil(entry.price * quantity); // Calculate amount dynamically
     data.amount = finalAmount; // Override the amount field
 
     if (!address || !isConnected) {
@@ -84,7 +84,7 @@ export function ColumnActions({ entry }: { entry: StockData }) {
         mpesa_request_id: mpesa_request_id,
         user_wallet: address,
         purchase_date: new Date(),
-        transaction_type: "buy"
+        transaction_type: "buy",
       });
       await updateUserStockHoldings({
         stock_symbol: data.stock_symbol,
