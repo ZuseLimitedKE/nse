2c09ab1a2a141ef4aa02f4fcf112b8c0c45ba299
Roman Njoroge <roman.njoroge@njuguna.com>
2025-03-31 14:54:44 +0300

    feat: Database function for getting token from symbol

--- site/src/constants/errors.ts
+++ site/src/constants/errors.ts
@@ -46,5 +46,6 @@ export enum Errors {
     NOT_GET_GRAPH_DATA = "Could not get graph data",
     NOT_UPDATE_PURCHASE_STATUS_DB = "Could not update sale status in db",
     NOT_GET_MPESA_PAYMENT = "Could not get payment from mpesa request id",
+    NOT_GET_STOCK_DB = "Could not get stock from DB",
     UNKNOWN = "Unkown error",
 }
