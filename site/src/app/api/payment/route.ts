62eb4895a352f961f476cfbde5fd3ce9470c81f3
Roman Njoroge <roman.njoroge@njuguna.com>
2025-03-31 15:48:55 +0300

    feat: Marking payment as paid if succesful

--- site/src/app/api/payment/route.ts
+++ site/src/app/api/payment/route.ts
@@ -1,12 +1,23 @@
-export async function GET(request: Request) {
-  console.log(request, "GET request");
-  return Response.json({ message: "Okay" }, { status: 200 });
-}
+import markRequestAsPaid from "@/server-actions/mpesa/markPaid";
 
 export async function POST(request: Request) {
-  console.log(request, "POST request");
-  console.log(request.body);
-  console.log(await request.json())
-  console.log("Anthony sucks")
+  // Check if the payment was succesful
+  const data = await request.json();
+  if (!data.Body.stkCallback.CallbackMetadata) {
+    // Not succesful
+    console.log("Payment request was not succesful");
+    return Response.json({ message: "Okay" }, { status: 200 });
+  }
+
+  // Succesful response
+  const mpesa_id = data.Body.stkCallback.MerchantRequestID;
+  if (mpesa_id) {
+    try {
+      await markRequestAsPaid(mpesa_id);
+    } catch(err) {
+      console.log("Error marking payment as paid", err);
+    }
+  }
+
   return Response.json({ message: "Okay" }, { status: 200 });
 }
