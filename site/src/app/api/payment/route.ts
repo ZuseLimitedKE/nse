3189292bab9eb11ad2ce8c40382639fcc8617684
Roman Njoroge <roman.njoroge@njuguna.com>
2025-03-31 14:07:54 +0300

    build: More logs

--- site/src/app/api/payment/route.ts
+++ site/src/app/api/payment/route.ts
@@ -5,5 +5,8 @@ export async function GET(request: Request) {
 
 export async function POST(request: Request) {
   console.log(request, "POST request");
+  console.log(request.body);
+  console.log(await request.json())
+  console.log("Anthony sucks")
   return Response.json({ message: "Okay" }, { status: 200 });
 }
