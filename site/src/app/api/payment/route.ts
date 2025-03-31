0f5a2509789f69a5acb67188f3b89223faf8b2ce
Roman Njoroge <roman.njoroge@njuguna.com>
2025-03-31 11:33:22 +0300

    feat: Routes

--- site/src/app/api/payment/route.ts
+++ site/src/app/api/payment/route.ts
@@ -1,4 +1,9 @@
 export async function GET(request: Request) {
-  console.log(request);
+  console.log(request, "GET request");
+  return Response.json({ message: "Okay" }, { status: 200 });
+}
+
+export async function POST(request: Request) {
+  console.log(request, "POST request");
   return Response.json({ message: "Okay" }, { status: 200 });
 }
