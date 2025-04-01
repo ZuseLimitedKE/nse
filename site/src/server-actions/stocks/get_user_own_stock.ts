71e8463a0e0f314aa319ea5ad39e8873edf35cef
Roman Njoroge <roman.njoroge@njuguna.com>
2025-04-01 18:16:14 +0300

    feat: Function to get if user owns stock

---
+++ site/src/server-actions/stocks/get_user_own_stock.ts
@@ -0,0 +1,27 @@
+"use server"
+
+import { Errors, MyError } from "@/constants/errors";
+import database from "@/db"
+import "../../../envConfig";
+
+export async function getIfUserHasOwnedStock(address: string, symbol: string): Promise<boolean> {
+    try {
+        // Get stocks owned by user
+        const stocks = await database.getStocksOwnedByUser(address);
+
+        if (!stocks || !stocks.stocks) {
+            return false;
+        }
+
+        for (let s of stocks.stocks) {
+            if (s.symbol === symbol) {
+                return true;
+            }
+        }
+
+        return false;
+    } catch(err) {
+        console.log("Error getting if user owned stock");
+        throw new MyError(Errors.NOT_GET_USER_OWN_STOCK);
+    }
+}
