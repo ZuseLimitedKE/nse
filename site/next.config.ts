0cbe6f44a9d7034adcbe1674f8bbc90079903d1c
mbeka02 <antonymbeka@gmail.com>
2025-04-10 21:52:32 +0300

    updated config , disabled strict mode

--- site/next.config.ts
+++ site/next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  reactStrictMode: false,
   /* config options here */
   compiler: {
     // Remove console logs only in production
